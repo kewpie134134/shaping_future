9f1f3d89a1b308c47f4201b0798c0c3b0623bfdc
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2019-06-30 19:38:24 +0900

    auth token fix2

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -3,7 +3,7 @@
 # SlackのAPIを利用するためのトークン
 # Botの設定ページから「OAuth & Permissions」のページに遷移し、
 # 「Bot User OAuth Access Token」をコピーして貼り付ける
-API_TOKEN = "xoxb-274122834004-682289922343-pv6KAyhLx3Vn4SXABW72tHcE"
+API_TOKEN = "xoxb-274122834004-682289922343-eAVAETLymnmsOlp31vgYl1GG"
 
 # 対応するメッセージがなかった場合に反応するメッセージ
 DEFAULT_REPLY = "すみません、良い返事ができませんでした。"
